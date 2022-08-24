58b374a338afccf38c3819362fca4cd66346a6a0
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-08-24 15:52:15 +0200

    Nodemon and express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//Start Server
+app.listen(3000, () => console.log("Server started on 3000"));
