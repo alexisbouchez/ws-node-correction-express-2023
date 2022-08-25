75a044345733d3e971da23fa3fde3808d8887546
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-08-25 13:36:51 +0200

    Async await instead of then

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -2,42 +2,41 @@ const dataSource = require("../utils").dataSource;
 const Wilder = require("../entity/Wilder");
 
 module.exports = {
-  create: (req, res) => {
-    dataSource
-      .getRepository(Wilder)
-      .save(req.body)
-      .then(() => {
-        res.send("Created wilder");
-      })
-      .catch(() => {
-        res.send("Error while creating wilder");
-      });
+  create: async (req, res) => {
+    try {
+      await dataSource.getRepository(Wilder).save(req.body);
+      res.send("Created wilder");
+    } catch (error) {
+      console.log(error);
+      res.send("Error while creating wilder");
+    }
   },
-  read: (req, res) => {
-    dataSource
-      .getRepository(Wilder)
-      .find()
-      .then((data) => {
-        res.send(data);
-      })
-      .catch(() => {
-        res.send("error while querying wilders");
-      });
+  read: async (req, res) => {
+    try {
+      await dataSource.getRepository(Wilder).find();
+      res.send(data);
+    } catch (error) {
+      res.send("error while querying wilders");
+    }
   },
-  delete: (req, res) => {
-    dataSource
-      .getRepository(Wilder)
-      .delete(req.body)
-      .then(() => {
-        res.send("deleted");
-      });
+  delete: async (req, res) => {
+    try {
+      await dataSource.getRepository(Wilder).delete(req.body);
+      res.send("deleted");
+    } catch (error) {
+      console.log(error);
+      res.send("error while deleting wilder");
+    }
   },
-  update: (req, res) => {
-    dataSource
-      .getRepository(Wilder)
-      .update(req.body.id, req.body.newData)
-      .then(() => {
-        res.send("Updated");
-      });
+  update: async (req, res) => {
+    try {
+      await dataSource
+        .getRepository(Wilder)
+        .update(req.body.id, req.body.newData);
+      res.send("Updated");
+    } catch (error) {
+      console.log(error);
+      res.send("error while updating wilder");
+    }
   },
 };
